7f242914e2ca97abb31d68c99f589fcc820d5dc9
andrey goncharov <goncharov.andrey.v@gmail.com>
2013-09-22 00:01:01 +0400

    Turn to the service.

---
+++ web_queue_client/web_queue_client/Default.aspx
@@ -0,0 +1,72 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="web_queue_client.WebForm1" %>
+
+<!DOCTYPE html>
+<meta http-equiv="REFRESH" content="10">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+	<title></title>
+	<link rel="stylesheet" href="css/bootstrap.min.css" />
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="css/bootstrap-theme.min.css" />
+
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="js/bootstrap.min.js"></script>
+	<script language="javascript">
+		function submit() {
+			window.SUBMIT();
+		}
+	</script>
+</head>
+<body>
+	<form id="form1" runat="server">
+		<div class="row" style="padding-top: 15px;">
+			<div class="col-lg-5" style="padding-left: 35px">
+				<label class="text-muted">Cервер:</label>
+				<label class="text-danger" style="font-size: large">localhost</label>
+			</div>
+		</div>
+		<div class="row" style="padding-bottom: 15px">
+			<div class="col-lg-5" style="padding-left: 35px">
+				<label class="text-muted">Состояние сервера:</label>
+				<strong>
+					<label runat="server" id="locked" class="text-danger" style="font-size: medium"></label>
+				</strong>
+				<label runat="server" id="free" class="text-success" style="font-size: medium"></label>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-lg-5" style="padding-left: 35px">
+				<label class="text-muted">Очередь:</label>
+				<table class="table table-hover table-bordered table-striped" id="queue" runat="server">
+				</table>
+			</div>
+		</div>
+
+		<div class="form-group">
+			<div class="row" style="padding-left: 20px; padding-bottom: 15px">
+				<div class="col-lg-5">
+					<select class="form-control" id="users" runat="server" onchange="submit()" onserverchange="UserChange">
+						<option>Федотов Сергей</option>
+						<option>Малышев Григорий</option>
+						<option>Гончаров Андрей</option>
+						<option>Диев Андрей</option>
+						<option>Радченко Анна</option>
+						<option>Викулина Дарья</option>
+						<option>Ешмеков Кирилл</option>
+						<option>Федосеев Ян</option>
+					</select>
+				</div>
+			</div>
+
+			<div class="row" style="padding-left: 35px;">
+				<input type="button" id="push" runat="server" value="Встать в очередь" class="btn btn-success" onserverclick="Push" />
+				<input type="button" id="pop" runat="server" value="Выйти из очереди" class="btn btn-danger" onserverclick="Pop" />
+			</div>
+
+		</div>
+
+	</form>
+</body>
+</html>
---
+++ web_queue_client/web_queue_client/Default.aspx.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Web;
+using System.IO;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using web_queue_client.queue_srv;
+
+namespace web_queue_client
+{
+	public partial class WebForm1 : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			if (!IsPostBack)
+				Update(Session["index"] == null ? 0 : Convert.ToInt32(Session["index"]));
+		}
+
+		protected void Push(Object sender, EventArgs eventArgs)
+		{
+			var index = users.SelectedIndex;
+			var user = users.Items[index].Text;
+
+			var srvc = ConnectToService();
+
+			try
+			{
+				srvc.Enqueue(user);
+			}
+			catch (Exception ex)
+			{
+				//_lg.WriteErr("btnEnqueue_Click", ex);
+			}
+			finally
+			{
+				CloseConnection(srvc);
+			}
+
+			Update(index);
+			Session["index"] = index;
+		}
+
+		protected void UserChange(Object sender, EventArgs eventArgs)
+		{
+			var index = users.SelectedIndex;
+			Update(index);
+			Session["index"] = index;
+		}
+
+		protected void Pop(Object sender, EventArgs eventArgs)
+		{
+			var index = users.SelectedIndex;
+			var user = users.Items[index].Text;
+
+			var srvc = ConnectToService();
+
+			try
+			{
+				srvc.DeleteFromQueue(user);
+			}
+			catch (Exception ex)
+			{
+				//_lg.WriteErr("btnGoOutFromQueue_Click", ex);
+			}
+			finally
+			{
+				CloseConnection(srvc);
+			}
+
+			Update(index);
+			Session["index"] = index;
+		}
+
+		private void Update(int selectedIndex)
+		{
+			users.SelectedIndex = selectedIndex;
+
+			RdpState state;
+
+			var srvc = ConnectToService();
+
+			try
+			{
+				state = srvc.GetRdpState();
+			}
+			catch (Exception ex)
+			{
+				//_lg.WriteErr("UpdateData", ex);
+				//lblServerState.Text = Resources.NoConnection;
+				return;
+			}
+			finally
+			{
+				CloseConnection(srvc);
+			}
+
+
+			FormState(state);
+
+			if (state.Queue.Count == 0)
+			{
+				var row = new HtmlTableRow();
+				queue.Rows.Add(row);
+
+				var c1 = new HtmlTableCell { InnerText = "Очередь пуста." };
+				row.Cells.Add(c1);
+				return;
+
+			}
+
+			for (int i = 0; state.Queue.Count > 0; ++i)
+			{
+				var row = new HtmlTableRow();
+				queue.Rows.Add(row);
+
+				var c1 = new HtmlTableCell { InnerText = (1 + i).ToString() };
+				row.Cells.Add(c1);
+
+				var c2 = new HtmlTableCell { InnerText = state.Queue.Dequeue() };
+				row.Cells.Add(c2);
+			}
+		}
+
+		private void FormState(RdpState state)
+		{
+			if(state.Free)
+			{
+				locked.InnerText = String.Empty;
+
+				free.InnerText = "Свободен.";
+			}
+			else
+			{
+				locked.InnerText = state.CurrentLocker + " на сервере.";
+				free.InnerText = String.Empty;
+			}
+
+		}
+
+		private static RdpQueueServiceClient ConnectToService()
+		{
+			var srvc = new RdpQueueServiceClient();
+
+			if (String.IsNullOrEmpty(ConfigurationManager.AppSettings["aut"]) || srvc.ChannelFactory.Credentials == null)
+				return srvc;
+
+			srvc.ChannelFactory.Credentials.Windows.ClientCredential.UserName = ConfigurationManager.AppSettings["userId"];
+			srvc.ChannelFactory.Credentials.Windows.ClientCredential.Password = ConfigurationManager.AppSettings["password"];
+			srvc.ChannelFactory.Credentials.Windows.ClientCredential.Domain = ConfigurationManager.AppSettings["dns"];
+
+			return srvc;
+		}
+
+		private static void CloseConnection(RdpQueueServiceClient srvc)
+		{
+			if (srvc == null)
+				return;
+
+			if (srvc.State == CommunicationState.Faulted)
+			{
+				srvc.Abort();
+			}
+			else
+			{
+				srvc.Close();
+			}
+		}
+	}
+}
